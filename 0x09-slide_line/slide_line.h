b41bb5e8120230b54f940e5d9a835dc24fbabce3
breezybrow <browder.bree@gmail.com>
2022-01-31 23:59:53 +0000

    Beginning project setup

---
+++ 0x09-slide_line/slide_line.h
@@ -0,0 +1,11 @@
+#ifndef _SLIDE_LINE_H
+#define _SLIDE_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif /*_SLIDE_LINE_H*/
