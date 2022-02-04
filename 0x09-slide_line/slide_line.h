74c2f7b2a7b4a4e49371cd8a121eac4f560a7a04
Bree (she/her) <browder.bree@gmail.com>
2022-02-04 00:31:38 -0600

    Update slide_line.h

--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -8,9 +8,6 @@
 #define SLIDE_LEFT - 1
 #define SLIDE_RIGHT + 1
 
-/* HELPERS */
-void slide_right(int *line, size_t size);
-void slide_left(int *line, size_t size);
 
 int slide_line(int *line, size_t size, int direction);
 
