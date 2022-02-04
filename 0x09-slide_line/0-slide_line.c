3e34415976b5cbe27528ab0f55b094217da4dcad
breezybrow <browder.bree@gmail.com>
2022-02-04 00:39:57 +0000

    not finished, but is compiling

---
+++ 0x09-slide_line/0-slide_line.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "slide_line.h"
+/**
+* slide_line - function that slides and merges an array of integers
+* @line: array of integers
+* @size: number of elements in array
+* @direction: slide left or slide right macro
+* 
+* Return: 1 upon success 0 upon failure
+*/
+int slide_line(int *line, size_t size, int direction)
+{
+        int tmp = line[0]; /* start array index of 0 */
+	unsigned int idx; /* iterator */
+	int flag = 0; /* boolean value false */
+
+	if (size < 1)
+	{
+	  return (flag);
+	}
+	
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+	{
+	  return (flag);
+	}
+
+	if (line == NULL)
+		return (0);
+
+	if (direction == SLIDE_LEFT)
+	{
+		for (idx = 0; idx < size - 1; idx++)
+			line[idx] = line[idx + 1];
+	}
+
+	if (direction == SLIDE_RIGHT)
+	{
+		tmp = line[size - 1];
+		for (idx = size - 1; idx > 0; i--)
+			line[idx] = line[idx - 1];
+		line[0] = tmp;
+	}
+	return (1);
+}
--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+#define SLIDE_LEFT - 1
+#define SLIDE_RIGHT + 1
 
 int slide_line(int *line, size_t size, int direction);
 
