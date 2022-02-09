d24402d3741f86804f4d1ffcaa06d4ed955477b3
breezybrow <browder.bree@gmail.com>
2022-02-09 20:21:25 +0000

    Update header file

--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -8,6 +8,10 @@
 #define SLIDE_LEFT - 1
 #define SLIDE_RIGHT + 1
 
+/* HELPER FUNCTIONS */
+void slide_left(int *line, size_t size);
+void slide_right(int *line, size_t size);
+
 
 int slide_line(int *line, size_t size, int direction);
 
