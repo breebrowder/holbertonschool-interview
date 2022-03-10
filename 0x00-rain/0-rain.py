572ced2acb5ad69c218822086955dcbee68ccf72
breezybrow <browder.bree@gmail.com>
2022-03-10 17:55:12 +0000

    Changes to solution

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -6,10 +6,11 @@
 
 
 def rain(walls):
-    """ 
+    """
     walls is a list of non-negative integers
     Return: Integer indicating total amount of rainwater retained.
-    Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water
+    Assume that the ends of the list (before index 0 and after index
+    walls[-1]) are not walls, meaning they will not retain water
     If the list is empty return 0
 
     """
@@ -20,5 +21,4 @@ def rain(walls):
         left = max(walls[:i + 1])
         right = max(walls[i:])
         waterRetained += max(min(left, right) - walls[i], 0)
-    
-    return  waterRetained
+    return waterRetained
