d2b37fc3cf5dd3a8103cc61d874f9c21db23c896
breezybrow <browder.bree@gmail.com>
2022-03-10 19:50:05 +0000

    clean up add comments

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -13,12 +13,23 @@ def rain(walls):
     walls[-1]) are not walls, meaning they will not retain water
     If the list is empty return 0
 
-    """
+    0 is the ground level
+    iterate through the length of the entire list
+    move through the left list going through each index
+    at each index, track the height and keep moving
+    when new index is a positive integer add to amount of water retained
+    maximum level in the right list is 4
+
+   """
 
     waterRetained = 0
 
-    for i in range(len(walls)):
-        left = max(walls[:i + 1])
-        right = max(walls[i:])
-        waterRetained += max(min(left, right) - walls[i], 0)
+    if not walls:
+        return 0
+
+    for idx in range(len(walls)):
+        left = max(walls[:idx + 1])
+        right = max(walls[idx:])
+
+        waterRetained += max(min(left, right) - walls[idx], 0)
     return waterRetained
