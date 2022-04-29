bdf51ca853d774d81d64e96dac9afc08549bb7fa
breezybrow <browder.bree@gmail.com>
2022-04-28 23:33:00 -0500

    pep8

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -15,11 +15,13 @@ def makeChange(coins, total):
 
     Len = len(coins)
     for idx in range(1, total+1):
-        smallest = float("inf") #  positive infinity
+         #  positive infinity
+        smallest = float("inf")
         for y in range(0, Len):
             if (coins[y] <= idx):
                 smallest = min(smallest, Tmp[idx - coins[y]])
         Tmp[idx] = 1 + smallest
-    if smallest == float("inf"): #  positive infinity
+         #  positive infinity
+    if smallest == float("inf"):
         return -1
     return Tmp[total]
