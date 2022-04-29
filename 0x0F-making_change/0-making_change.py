62fcefaa7f263c68f088dfe06aeef0b9802c4b25
breezybrow <browder.bree@gmail.com>
2022-04-28 23:34:01 -0500

    edits

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -6,7 +6,7 @@ makeChange function
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total
+    determine the fewest number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
