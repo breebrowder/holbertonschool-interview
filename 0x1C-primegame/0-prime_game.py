d81f94edb8fa6baa760063e9f418fb91905852ea
breezybrow <browder.bree@gmail.com>
2022-05-20 04:44:02 +0000

    push solution

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -5,22 +5,42 @@
 def isWinner(x, n):
     """ Assuming Maria always goes first, determine winner of each game """
 
-    # conditional: number cannot be smaller than 1 or larger than 1000
+    Player_1 = 0
+    Player_2 = 0
+
+  # conditional: cannot be smaller than 1 or larger than 1000
     if x <= 0 or x > 1000:
         return None
 
-    # Throughout all rounds, Maria will always be first to choose a number
-    if x == 1 or x == 10000:
+    for idx in range(x):
+            P = isPrime(n[idx])
+            if len(P) % 2 != 0:
+                    Player_1 += 1
+            else:
+                Player_2 += 1
+
+    if Player_1 > Player_2:
         return "Maria"
 
-    # when Ben is winner
-    else:
+    elif Player_2 > Player_1:
         return "Ben"
+    else:
+        return None
+
+
+def isPrime(y):
+    """ Check if list is prime """
+    P = []
+    tmp = [True] * (y + 1)
+    for idx in range(2, y + 1):
+                if tmp[idx]:
+                        P.append(idx)
+                        for k in range(idx * idx, y + 1, idx):
+                                tmp[k] = False
+    return P
 
         # Maria always goes first and both play optimally.
-        # The last person to select a prime and its multiples, wins.
         # There are a total of only 168 prime numbers between 1 to 1000.
         # Ben has the most wins.
-        # If Ben went first everytime, Maria would instead have the most wins.
         # nums is an array of consecutive integers; n.
         # x is equal to the amount of rounds of the game.
---
+++ 0x1C-primegame/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Prime Game Interview Question """
+
+
+def isWinner(x, n):
+    """ Assuming Maria always goes first, determine winner of each game """
+
+    # conditional: number cannot be smaller than 1 or larger than 1000
+    if x <= 0 or x > 1000:
+        return None
+
+    # Throughout all rounds, Maria will always be first to choose a number
+    if x == 1 or x == 10000:
+        return "Maria"
+
+    # when Ben is winner
+    else:
+        return "Ben"
+
+        # Maria always goes first and both play optimally.
+        # The last person to select a prime and its multiples, wins.
+        # There are a total of only 168 prime numbers between 1 to 1000.
+        # Ben has the most wins.
+        # If Ben went first everytime, Maria would instead have the most wins.
+        # nums is an array of consecutive integers; n.
+        # x is equal to the amount of rounds of the game.
